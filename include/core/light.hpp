c36daa5a1d6c17ed6bf74d2f62ac4ab4024c6834
Yann LE PETITCORPS <yannoo95170@gmail.com>
2026-05-24 19:56:21 +0200

    Update

--- include/core/light.hpp
+++ include/core/light.hpp
@@ -8,34 +8,33 @@ class Light {
 public:
     enum class Type { DIRECTIONAL, POINT, SPOT };
 
-    Light(Type type = Type::DIRECTIONAL)
-        : m_type(type), m_color(1.0f, 1.0f, 1.0f), m_intensity(1.0f) {}
+    Light(Type type = Type::DIRECTIONAL);
 
-    Type getType() const { return m_type; }
-    void setType(Type type) { m_type = type; }
+    Type getType() const;
+    void setType(Type type);
 
-    const Vec3& getColor() const { return m_color; }
-    void setColor(const Vec3& color) { m_color = color; }
+    const Vec3& getColor() const;
+    void setColor(const Vec3& color);
 
-    float getIntensity() const { return m_intensity; }
-    void setIntensity(float intensity) { m_intensity = intensity; }
+    float getIntensity() const;
+    void setIntensity(float intensity);
 
-    const Vec3& getDirection() const { return m_direction; }
-    void setDirection(const Vec3& direction) { m_direction = direction.Normalize(); }
+    const Vec3& getDirection() const;
+    void setDirection(const Vec3& direction);
 
-    const Vec3& getPosition() const { return m_position; }
-    void setPosition(const Vec3& position) { m_position = position; }
+    const Vec3& getPosition() const;
+    void setPosition(const Vec3& position);
 
-    float getRange() const { return m_range; }
-    void setRange(float range) { m_range = range; }
+    float getRange() const;
+    void setRange(float range);
 
 private:
     Type m_type;
     Vec3 m_color;
     float m_intensity;
-    Vec3 m_direction = Vec3(0.0f, -1.0f, 0.0f);
+    Vec3 m_direction;
     Vec3 m_position;
-    float m_range = 10.0f;
+    float m_range;
 };
 
 } // namespace ygl
